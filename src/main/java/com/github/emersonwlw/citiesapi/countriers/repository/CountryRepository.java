33654b2ec4ddae8f1fb0d9ddfa210a10f0d254f3
Emerson Willian <emersonwlw@gmail.com>
2022-08-10 22:01:45 -0300

    first commit up files

---
+++ src/main/java/com/github/emersonwlw/citiesapi/countriers/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package com.github.emersonwlw.citiesapi.countriers.repository;
+
+import com.github.emersonwlw.citiesapi.countriers.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long> {
+
+
+}
---
+++ src/main/java/com/github/emersonwlw/citiesapi/staties/StateRepository.java
@@ -0,0 +1,8 @@
+package com.github.emersonwlw.citiesapi.staties;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StateRepository extends JpaRepository<State, Long> {
+
+
+}
